567d685ab1c5f69ad23e1dc3d047c6bf24b3f230
Pro-Coder-Aditya <mungekar.aditya15@gmail.com>
2025-06-19 16:34:59 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName;
+let botLocation;
+let favoriteLanguage;
+
+botName = "Winnie";
+botLocation = "The universe";
+favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName+ " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "The bot's favorite language is " + favoriteLanguage + ".";
+console.log(codingFact);
+
+codingFact = "I really love" + favoriteLanguage + ".";
+console.log(codingFact);
+
+codingFact = "My favorite language is " + favoriteLanguage + ".";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! " + botName + " from " + botLocation + ".");
